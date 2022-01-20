d15e3693f12f8559b5b80d947db03b7cbb4a1000
KEVIN LAMARCA <LAMARCA.K@GMAIL.COM>
2022-01-20 17:55:37 -0500

    real time weather forecasts rendering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Weather from './Weather.js';
 
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -40,7 +41,7 @@ class App extends React.Component {
         mapUrl: `https://maps.locationiq.com/v3/staticmap?key=pk.09705190e7b42d59adba58923c8dbfa4&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=13`,
         renderMap: true,
       })
-      let serverUrl = `https://city-explorers-api-kevdev.herokuapp.com/weather?searchQuery=${this.state.userSearch}&lat=${this.state.locationData.lat}&lon=${this.state.locationData.lon}`;
+      let serverUrl = `${process.env.REACT_APP_SERVER_URL}/weather?lat=${this.state.locationData.lat}&lon=${this.state.locationData.lon}`;
       let weatherData = await axios.get(serverUrl);
       console.log(weatherData)
       this.setState({
@@ -67,72 +68,54 @@ class App extends React.Component {
             <Header title='City Explorers' subtitle='View a map of your favorite city in real time' />
           </div>
           <div className="main">
-            <main>
-              <form onSubmit={this.getCityInfo}>
-                <label>Enter a City!
-                  <input type="text" name="city" onInput={this.handleSubmit}></input>
-                </label>
-                <button type="submit">Submit</button>
-              </form>
-              <div className="map">
-                {
-                  this.state.renderMap && <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={this.state.mapUrl} />
-                    <Card.Body>
-                      <Card.Title>{this.state.locationData.display_name}</Card.Title>
-                      <Card.Text>
-                        <p>Latitude: {this.state.locationData.lat}</p>
-                        <p>longitude: {this.state.locationData.lon}</p>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                }
+            <form onSubmit={this.getCityInfo}>
+              <label>Enter a City!
+                <input type="text" name="city" onInput={this.handleSubmit}></input>
+              </label>
+              <button type="submit">Submit</button>
+            </form>
+
+            {
+              this.state.renderMap && <div className="map"><Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.state.mapUrl} />
+                <Card.Body>
+                  <Card.Title>{this.state.locationData.display_name}</Card.Title>
+                  <Card.Text>
+                    <p>Latitude: {this.state.locationData.lat}</p>
+                    <p>longitude: {this.state.locationData.lon}</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
               </div>
-                <div className="forecasts">
-                {
-                  this.state.renderWeather && <Weather
-                    date={this.state.weatherData[0].date}
-                    description={this.state.weatherData[0].description}
-                    lowTemp={this.state.weatherData[0].lowTemp}
-                    maxTemp={this.state.weatherData[0].maxTemp}
-                  />
-                }
-                </div>
-                <div className="forecasts">
-                {
-                  this.state.renderWeather && <Weather
-                    date={this.state.weatherData[1].date}
-                    description={this.state.weatherData[1].description}
-                    lowTemp={this.state.weatherData[1].lowTemp}
-                    maxTemp={this.state.weatherData[1].maxTemp}
-                  />
-                }
-                </div>
-              <div className="forecasts">
-                {
-                  this.state.renderWeather && <Weather
-                    date={this.state.weatherData[2].date}
-                    description={this.state.weatherData[2].description}
-                    lowTemp={this.state.weatherData[2].lowTemp}
-                    maxTemp={this.state.weatherData[2].maxTemp}
-                  />
-                }
+            }
+            {
+              this.state.renderWeather ?
+                this.state.weatherData.map((obj, idx) => {
+                  return (
+                    <div className="listItem">
+                      <Weather
+                        date={obj.date}
+                        description={obj.description}
+                        temp={obj.temp}
+                        key={idx}
+                      />
+                    </div>
+                  )
+                }) : null
+            }
+            {
+              this.state.errorEvent && <div className="errorMsg"><Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Ya dun goofed kiddo</Card.Title>
+                  <Card.Text>
+                    <p>{this.state.errorMsg}</p>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
               </div>
-              <div className="errorMsg">
-                {
-                  this.state.errorEvent && <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                      <Card.Title>Ya dun goofed kiddo</Card.Title>
-                      <Card.Text>
-                        <p>{this.state.errorMsg}</p>
-                      </Card.Text>
-                    </Card.Body>
-                  </Card>
-                }
-              </div>
-            </main>
+            }
           </div>
-        </body>
+        </body >
       </>
     )
   }
